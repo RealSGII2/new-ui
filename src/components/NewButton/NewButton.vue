303fe62d829738c4105d7bf82cca04b9f9859055
RealSGII2 <rbxsgii2@gmail.com>
2020-01-18 13:07:11 -0800

    a

--- src/components/NewButton/NewButton.vue
+++ src/components/NewButton/NewButton.vue
@@ -1,18 +1,47 @@
 <template>
-  <button class="test">
+  <button v-bind:class="class">
     <slot></slot>
   </button>
 </template>
 
 <script>
   export default {
-    name: 'NewButton'
+    name: 'NewButton',
+    props: {
+      style: {
+        type: String,
+        default: 'default',
+        validator: function (value) {
+          return ['default', 'pill', 'tile'].indexOf(value) !== -1
+        }
+      }
+      expanded: {
+        type: Boolean,
+        default: false
+      }
+      appearance: {
+        type: String,
+        default: 'text',
+        validator: function (value) {
+          return ['stroked', 'filled', 'text'].indexOf(value) !== -1
+        }
+      },
+      state: {
+        type: String,
+        default: 'default',
+        validator: function (value) {
+          return ['info', 'success', 'warn', 'error', 'default', 'accent', 'brand'].indexOf(value) !== -1
+        }
+      }
+    },
+    computed: {
+      class: function() {
+        let returned =  `new-ui new-button new-button--${this.state}--${this.appearance} new-button--${if (this.style!=='default'){this.style}} new-button--${if (this.expanded!==false){'expanded'}}`;
+      }
+    }
   }
 </script>
 
 <style lang="scss">
   @import '../../styles/styles.scss';
-  .test {
-    background-color: #ccffff;
-  }
 </style>
