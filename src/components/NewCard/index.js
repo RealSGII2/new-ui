a399c2b28d45190489c15138c16a37cbde0a7b0f
RealSGII2 <rbxsgii2@gmail.com>
2020-01-18 15:53:27 -0800

    a

--- src/components/NewCard/index.js
+++ src/components/NewCard/index.js
@@ -1,7 +1,13 @@
 import NewCard from './NewCard'
+import NewCardContent from './NewCardContent/NewCardContent'
+import NewCardHeader from './NewCardHeader/NewCardHeader'
+import NewCardHeaderText from './NewCardHeader/NewCardHeaderText'
 
 const exported = Vue => {
   Vue.component(NewCard.name, NewCard)
+  Vue.component(NewCardContent.name, NewCardContent)
+  Vue.component(NewCardHeader.name, NewCardHeader)
+  Vue.component(NewCardHeaderText.name, NewCardHeaderText)
 }
 
 export default exported
