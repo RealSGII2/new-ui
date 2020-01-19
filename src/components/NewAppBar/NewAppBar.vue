68eabd81577dd27ad270420f8a96206b5068c645
RealSGII2 <rbxsgii2@gmail.com>
2020-01-18 22:59:24 -0800

    add appbar and finishing touches

---
+++ src/components/NewAppBar/NewAppBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="new-ui new-appbar" :class="[stateClass, {'new-appbar--collapsed': collapsed}]">
+    <div class="new-ui new-appbar__menu-button">
+
+    </div>
+    <div class="new-ui new-appbar__title">
+      <h3>
+        {{ title }}
+      </h3>
+    </div>
+    <div class="new-ui new-appbar__actions">
+      <slot name="actions"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NewAppBar',
+    props: {
+      title: {
+        type: String,
+        default: 'My NewUI App'
+      },
+      state: {
+        type: String,
+        default: 'default',
+        validator: function (value) {
+          return ['info', 'success', 'warn', 'error', 'default', 'accent', 'brand'].indexOf(value) !== -1
+        }
+      },
+      collapsed: {
+        type: Boolean,
+        default: false
+      }
+    },
+    computed: {
+      stateClass() {
+        return `new-appbar--state-${this.state}`
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '../../styles/styles.scss';
+</style>
