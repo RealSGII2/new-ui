a4c7b4bc2f848c8d4cbe014741c1322e5a32a2c8
RealSGII2 <rbxsgii2@gmail.com>
2020-01-20 14:10:41 -0800

    Export NewCardActions

--- src/components/NewCard/index.js
+++ src/components/NewCard/index.js
@@ -1,4 +1,5 @@
 import NewCard from './NewCard'
+import NewCardActions from './parts/NewCardContent/NewCardActions'
 import NewCardContent from './parts/NewCardContent/NewCardContent'
 import NewCardContentSection from './parts/NewCardContentSection/NewCardContentSection'
 import NewCardHeader from './parts/NewCardHeader/NewCardHeader'
@@ -6,6 +7,7 @@ import NewCardImage from './parts/NewCardImage/NewCardImage'
 
 const exported = Vue => {
   Vue.component(NewCard.name, NewCard)
+  Vue.component(NewCardActions.name, NewCardActions)
   Vue.component(NewCardContent.name, NewCardContent)
   Vue.component(NewCardContentSection.name, NewCardContentSection)
   Vue.component(NewCardHeader.name, NewCardHeader)
