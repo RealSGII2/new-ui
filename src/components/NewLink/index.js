25ff1823a9fd2b1a7979c58aeddfd7ddc60dcdfb
RealSGII2 <rbxsgii2@gmail.com>
2020-01-19 16:07:47 -0800

    a

--- src/components/NewLink/index.js
+++ src/components/NewLink/index.js
@@ -1,7 +1,7 @@
-import NewButton from './NewButton'
+import NewLink from './NewLink'
 
 const exported = Vue => {
-  Vue.component(NewButton.name, NewButton)
+  Vue.component(NewLink.name, NewLink)
 }
 
 export default exported
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import NewAppBar from './NewAppBar'
 import NewButton from './NewButton'
 import NewButtonToolbar from './NewButtonToolbar'
 import NewCard from './NewCard'
+import NewLink from '/NewLink'
 
 export {
   NewAppBar,
   NewButton,
   NewButtonToolbar,
-  NewCard
+  NewCard,
+  NewLink
 }
